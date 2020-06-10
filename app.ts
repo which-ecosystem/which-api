b0daa9b051c9f44266fc4cbd1fbe8b704fe1fc81
eug-vs <eug-vs@keemail.me>
2020-06-10 19:44:42 +0300

    refactor: reorder calls in app.ts

--- app.ts
+++ app.ts
@@ -12,10 +12,11 @@ const app = express(feathers());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname));
-app.configure(express.rest());
-app.configure(socketio());
 app.use(express.errorHandler());
 app.use(cors());
+
+app.configure(express.rest());
+app.configure(socketio());
 app.configure(services);
 
 
