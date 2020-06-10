57275beb2ee3224994386ce616f86dbee292e2ee
eug-vs <eug-vs@keemail.me>
2020-06-10 18:41:46 +0300

    chore: install cors, bluebird and logash w/ types

--- app.ts
+++ app.ts
@@ -2,6 +2,7 @@ import feathers from '@feathersjs/feathers';
 import express from '@feathersjs/express';
 import socketio from '@feathersjs/socketio';
 import '@feathersjs/transport-commons';
+import cors from 'cors';
 
 import services from './services';
 
@@ -14,6 +15,7 @@ app.use(express.static(__dirname));
 app.configure(express.rest());
 app.configure(socketio());
 app.use(express.errorHandler());
+app.use(cors());
 app.configure(services);
 
 
