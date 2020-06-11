72a89f5eff0a7b93d5cc5cf81255b9a1efcf20f1
eug-vs <eug-vs@keemail.me>
2020-06-11 20:03:35 +0300

    refactor: improve script

--- populateDb.ts
+++ populateDb.ts
@@ -1,28 +1,24 @@
 import mongoose from 'mongoose';
+import Promise from 'bluebird';
 import app from './app';
 
 mongoose.connect('mongodb://localhost:27017/which', { useNewUrlParser: true });
 
-import UserModel from './models/users/user.model';
-(async () => {
-  const users = [
-    { name: "Emma" },
-    { name: "Elise" },
-    { name: "Jack" },
-    { name: "Oliver" },
-    { name: "Jamie" },
-    { name: "Aidan" },
-    { name: "Jordan" },
-    { name: "Erin" },
-    { name: "William" },
-    { name: "Ethan" },
-  ];
-  try {
-    for (const user of users) {
-      await UserModel.create(user);
-    }
-  } catch (e) {
-    console.error(e);
-  }
-  mongoose.disconnect();
-})();
+const users = [
+  { name: "Emma" },
+  { name: "Elise" },
+  { name: "Jack" },
+  { name: "Oliver" },
+  { name: "Jamie" },
+  { name: "Aidan" },
+  { name: "Jordan" },
+  { name: "Erin" },
+  { name: "William" },
+  { name: "Ethan" },
+];
+
+Promise.map(users, async user => {
+  return await app.service('users').create(user);
+}).catch(e => console.error(e))
+  .finally(() => mongoose.disconnect());
+
