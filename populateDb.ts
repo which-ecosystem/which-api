2b876318d2aeca5183e0a57277904c7332a7aa0e
ilyayudovin <ilyayudovin123@gmail.com>
2020-06-11 21:19:18 +0300

    feat: add populate function

--- populateDb.ts
+++ populateDb.ts
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';
 import Promise from 'bluebird';
+import _ from 'lodash';
 import app from './app';
+import { UserSchema } from './models/users/user.schema';
 
 mongoose.connect('mongodb://localhost:27017/which', { useNewUrlParser: true });
 
@@ -17,8 +19,40 @@ const users = [
   { name: "Ethan" },
 ];
 
-Promise.map(users, async user => {
-  return await app.service('users').create(user);
-}).catch(e => console.error(e))
-  .finally(() => mongoose.disconnect());
+const polls = [{
+  contents: {
+    left:{
+      url: 'https://cdn.psychologytoday.com/sites/default/files/field_blog_entry_images/2019-06/pexels-photo-556667.jpeg',
+      votes: 0
+    },
+    right:{
+      url: 'https://images.pexels.com/photos/556666/pexels-photo-556666.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      votes: 0
+    }
+  }
+},{
+  contents: {
+    left:{
+      url: 'https://cdn.psychologytoday.com/sites/default/files/field_blog_entry_images/2019-06/pexels-photo-556667.jpeg',
+      votes: 0
+    },
+    right:{
+      url: 'https://images.pexels.com/photos/556666/pexels-photo-556666.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      votes: 0
+    }
+  }
+}];
 
+const populate = async () => {
+  const createdUsers = await Promise.map(users, async user => {
+    return await app.service('users').create(user);
+  });
+  console.log(createdUsers);
+  await Promise.map(polls, async poll => {
+    const user = _.sample(createdUsers);
+    return await app.service('polls').create({...poll, authorId: user._id});
+  });
+  mongoose.disconnect();
+};
+
+populate();
