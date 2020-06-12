24ff209193448ee047f428705879cef15e0b737a
eug-vs <eug-vs@keemail.me>
2020-06-12 19:30:00 +0300

    feat: finish profile services

--- services/profile/profile.service.ts
+++
@@ -1,7 +0,0 @@
-import { Application } from "@feathersjs/express";
-import Profiles from './profile.class';
-
-export default (app: Application): void => {
-  app.use('/profile', new Profiles());
-};
-
--- services/profiles/profiles.class.ts
+++ services/profiles/profiles.class.ts
@@ -3,7 +3,7 @@ import PollModel from '../../models/polls/poll.model';
 
 export default class Profiles {
   async get(id: string, params: any): Promise<PollSchema[]> {
-    return PollModel.find({ authorId: id });
+    return PollModel.find({ authorId: id }).lean<Poll>();
   }
 };
 
