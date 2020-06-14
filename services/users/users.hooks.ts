1ab199588d5353d98d49172e1f35d73452069faf
eug-vs <eug-vs@keemail.me>
2020-06-14 17:13:40 +0300

    feat: protect password field

--- services/users/users.hooks.ts
+++ services/users/users.hooks.ts
@@ -1,8 +1,12 @@
 import { hooks } from '@feathersjs/authentication-local';
 
 const hashPassword = hooks.hashPassword('password');
+const protectPassword = hooks.protect('password');
 
 export default {
+  after: {
+    all: [protectPassword]
+  },
   before: {
     create: [hashPassword],
     patch: [hashPassword],
