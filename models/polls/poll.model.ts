b054bd6cf5be0eed0c17fbacec8bcc09cf013d44
eug-vs <eug-vs@keemail.me>
2020-06-23 23:59:31 +0300

    fix: check user correctly

--- models/polls/poll.model.ts
+++ models/polls/poll.model.ts
@@ -1,13 +1,14 @@
 import { Model, model } from 'mongoose';
 import { PollSchema, pollSchema } from './poll.schema';
+import { Types } from 'mongoose';
 
 pollSchema.methods.vote = function(userId: string, which: 'left' | 'right'): PollSchema {
-  const participants = ['left', 'right'].reduce((acc, option) => {
+  const participants: Types.ObjectId[] = ['left', 'right'].reduce((acc, option) => {
     const { votes } = this.contents[option];
     return acc.concat(votes);
   }, []);
 
-  if (!participants.indexOf(userId) === -1) {
+  if (!participants.some(user => user.equals(userId))) {
     this.contents[which].votes.push(userId);
   }
 
