263ed426cc5ea79d36bca2b82a8aa858e43072eb
eug-vs <eug-vs@keemail.me>
2020-06-25 19:44:12 +0300

    feat: mark anonymous requests

--- hooks/logging.ts
+++ hooks/logging.ts
@@ -2,11 +2,12 @@ import { HookContext } from '@feathersjs/feathers';
 
 export default async (context: HookContext): Promise<HookContext> => {
   if (context.params.provider) {
-    const { method, path, id, params: { user: { username }} } = context;
+    const { method, path, id } = context;
     const timestamp = new Date().toLocaleString('default', { timeStyle: 'medium', dateStyle: 'short' });
     const message = `${method.toUpperCase()}: /${path}/${id || ''}`;
+    const username = context.params.user?.username || 'anonymous';
 
-    console.log(`[${timestamp}]  ${message} ${username || ''}`);
+    console.log(`[${timestamp}]  ${message} ${username}`);
   }
   return context;
 };
