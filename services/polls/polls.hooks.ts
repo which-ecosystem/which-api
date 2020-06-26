24376076e0b2c37baaf196f0664078840aeec91b
eug-vs <eug-vs@keemail.me>
2020-06-26 22:12:37 +0300

    feat: sort polls by date (newest on top)

--- services/polls/polls.hooks.ts
+++ services/polls/polls.hooks.ts
@@ -1,6 +1,16 @@
+import _ from 'lodash';
+import { HookContext } from '@feathersjs/feathers';
 import convertPoll from '../../hooks/convertPoll';
 
+const sort = async (context: HookContext): Promise<HookContext> => {
+  _.set(context, 'params.query.$sort', { createdAt: - 1});
+  return context;
+}
+
 export default {
+  before: {
+    find: sort
+  },
   after: {
     all: [convertPoll]
   }
