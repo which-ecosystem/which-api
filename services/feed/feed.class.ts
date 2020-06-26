7c2d12b4f809563cc08e86229359251674b745ce
eug-vs <eug-vs@keemail.me>
2020-06-27 01:17:42 +0300

    refactor: improve Feed service

--- services/feed/feed.class.ts
+++ services/feed/feed.class.ts
@@ -1,23 +1,13 @@
-import _ from 'lodash';
 import { Application } from '@feathersjs/express';
 import { Params } from '@feathersjs/feathers';
 import { Poll } from 'which-types';
-
-import { PollSchema } from '../../models/polls/poll.schema';
-import PollModel from '../../models/polls/poll.model';
 
 
 export default class Feed {
   app!: Application;
 
   async find(params: Params): Promise<Poll[]> {
-    return this.app.service('polls')
-      .find(params)
-      .then( // Move new verified polls on top
-        (polls: Poll[]) => _.sortBy(polls, poll => poll.author.verified && !poll.userChoice)
-      ).then( // But all seen posts go down
-        (polls: Poll[]) => _.sortBy(polls, poll => !!poll.userChoice)
-      );
+    return this.app.service('polls').find(params);
   }
 
   setup (app: Application) {
