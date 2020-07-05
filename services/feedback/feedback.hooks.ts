f38a1cf6a6809e85ac2ba47026c42b49bfee3673
eug-vs <eug-vs@keemail.me>
2020-07-05 12:23:11 +0300

    feat: upgrade feedback hooks

--- services/feedback/feedback.hooks.ts
+++ services/feedback/feedback.hooks.ts
@@ -1,9 +1,27 @@
+import { populate, discard } from 'feathers-hooks-common';
 import requireAuth from '../../hooks/requireAuth';
 import signAuthority from '../../hooks/signAuthority';
+import sortByDate from '../../hooks/sortByDate';
+
+
+const populateAuthor = populate({
+  schema: {
+    include: {
+      service: 'users',
+      nameAs: 'author',
+      parentField: 'authorId',
+      childField: '_id'
+    }
+  }
+});
 
 export default {
   before: {
-    create: [requireAuth, signAuthority]
+    create: [requireAuth, signAuthority],
+    find: sortByDate
+  },
+  after: {
+    all: [populateAuthor, discard('authorId')]
   }
 };
 
