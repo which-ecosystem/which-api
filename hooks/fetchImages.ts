763fd06f3076c96c7fa45ddd6efcc246520c8fbb
eug-vs <eug-vs@keemail.me>
2020-08-12 21:12:02 +0300

    fix: correctly reupload poll urls

--- hooks/fetchImages.ts
+++ hooks/fetchImages.ts
@@ -1,7 +1,9 @@
 import { HookContext } from '@feathersjs/feathers';
 import Bluebird from 'bluebird';
 import _ from 'lodash';
+import Debug from 'debug';
 
+const debug = Debug('s3-reuploads');
 
 export default (paths: string[]) => async (context: HookContext): Promise<HookContext> => {
   const {
@@ -19,10 +21,13 @@ export default (paths: string[]) => async (context: HookContext): Promise<HookCo
 
     // If image is not from our s3, fetch it!
     if (!fileService.isS3url(url)) {
+      debug('Found non-s3 url!');
       const filePath = await fileService.downloadFile(url);
       const s3Path = fileService.generateS3Path(user?.username);
       const s3Url = await fileService.uploadFileToS3(filePath, s3Path);
-      return model.findOneAndUpdate({ _id: result._id }, { [path]: s3Url });
+      await model.findOneAndUpdate({ _id: result._id }, { $set: { [path]: s3Url } });
+      debug(`Fetched and updated: from ${url} to ${s3Url}`);
+      return s3Url;
     }
     return url;
   });
